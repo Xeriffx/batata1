14823c83a6c9acf3032c7f2fbc0b55e5040ef98c
Xeriffx <122402439+Xeriffx@users.noreply.github.com>
2023-01-27 12:18:54 -0300

    Add files via upload

---
+++ potatohubP2.lua
@@ -0,0 +1,20 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Potato Hub v1 Page 2", "Ocean")
+
+-- MAIN
+local Main = Window:NewTab("read this")
+local MainSection = Main:NewSection("page 2 in development")
+
+-- MAIN
+local Main = Window:NewTab("page 3")
+local MainSection = Main:NewSection("not done yet srry")
+local MainSection = Main:NewSection("sus?")
+
+MainSection:NewButton("don't click pls!!", "you didn't click?", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/v6E9BmFK",true))();
+end)
+
+-- MAIN
+local Main = Window:NewTab("credits")
+local MainSection = Main:NewSection("made by jvnq_moreooo")
+local MainSection = Main:NewSection("my friend passed some scripts")
